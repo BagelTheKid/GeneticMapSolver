6078a1686202f28eb539794ea35ed9289608f4d4
Nicolas Polhamus <nicolas.polhamus@gmail.com>
2016-12-01 17:18:00 -0600

    Create map-analyzer.ts

---
+++ app/shared/map-analyzer.ts
@@ -0,0 +1,15 @@
+import { Tile } from './map/tile';
+import { Map } from './map/map';  //<-- Necessary?
+
+/**
+ * @author Nicolas Polhamus <polhamus.n@outlook.com>
+ * @exports 
+ * @class {Object} MapAnalyzer
+ */
+export class MapAnalyzer {
+    private map: Map;
+    constructor(m: Map) {
+        this.map = m;
+    }
+    
+}
